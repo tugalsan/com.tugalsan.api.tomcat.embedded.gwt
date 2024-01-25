0196235365cc8a8fa245f3b740388048f7bf41aa
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-01-26 01:47:45 +0300

    .

--- src/main/java/com/tugalsan/api/tomcat/embedded/gwt/server/servlets/TS_ServletDestroyByMapping.java
+++ src/main/java/com/tugalsan/api/tomcat/embedded/gwt/server/servlets/TS_ServletDestroyByMapping.java
@@ -3,12 +3,11 @@ package com.tugalsan.api.tomcat.embedded.gwt.server.servlets;
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-import com.tugalsan.api.log.server.*;
 import com.tugalsan.api.tomcat.embedded.gwt.server.*;
 
 public class TS_ServletDestroyByMapping extends TS_ServletAbstract {
 
-    final private static TS_Log d = TS_Log.of(TS_ServletDestroyByMapping.class);
+//    final private static TS_Log d = TS_Log.of(TS_ServletDestroyByMapping.class);
 
     public TS_ServletDestroyByMapping(TS_TomcatBall tomcatBall) {
         this.tomcatBall = tomcatBall;
